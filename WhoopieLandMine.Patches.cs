d98fc0112034a6829268084e720774574cad76ed
triston1228 <122327698+triston1228@users.noreply.github.com>
2024-07-09 21:42:11 -0400

    Add files via upload

---
+++ WhoopieLandMine.Patches.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace WhoopieLandMine
+{
+    [HarmonyPatch(typeof(GrabbableObject))]
+    public class WhoopieLandMinePatches
+    {
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        private static void Postfix(GrabbableObject __instance)
+        {
+            Debug.Log("GrabbableObject Patch Loading...");
+
+            if (__instance is WhoopieCushionItem)
+            {
+
+                Transform triggerTransform = __instance.transform.Find("Trigger");
+                if (triggerTransform != null)
+                {
+
+                    GameObject triggerObject = triggerTransform.gameObject;
+
+                    if (triggerObject.GetComponent<WhoopieScript>() == null)
+                    {
+                        triggerObject.gameObject.AddComponent<WhoopieScript>();
+                        Debug.Log("WhoopieScript Attached to child");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Child object named 'Trigger' not found");
+                }
+
+
+            }
+
+        }
+
+
+    }
+}
---
+++ WhoopieScript.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace WhoopieLandMine
+{
+    internal class WhoopieScript : NetworkBehaviour
+    {
+
+        public Vector3 whoopiePos;
+        public WhoopieCushionItem whoopieScript;
+
+        private void Start()
+        {
+            whoopieScript = GetComponent<WhoopieCushionItem>();
+        }
+        private void Update()
+        {
+            whoopiePos = transform.position;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!whoopieScript.isHeld && (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Enemy")))
+            {
+                Debug.Log("Explode");
+                Landmine.SpawnExplosion(whoopiePos, true, 2, 10, 50, 5, null, false);
+            }
+
+        }
+
+    }
+}
